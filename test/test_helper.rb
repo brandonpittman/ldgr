0b128dc0104aba97bfadccaa4fcccfc8f1bd4796
Brandon Pittman <brandon@brandonpittman.net>
2017-02-03 11:43:10 +0900

    Add tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ldgr'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
